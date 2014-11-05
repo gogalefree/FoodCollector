917c358a376844d60030f6cf3abd542537af0604
Guy Freedman <gogalefree@gmail.com>
2014-11-05 19:38:23 +0200

    Initial Commit

---
+++ FoodCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodCollector
+//
+//  Created by Guy Freedman on 11/5/14.
+//  Copyright (c) 2014 Guy Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
