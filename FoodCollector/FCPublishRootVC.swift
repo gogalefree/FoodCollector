8f28968c4d1d62ff337a81ee34507e57b0f8b824
Boris Tsigelman <boris@tsbmob.com>
2014-11-25 09:54:47 +0200

    All UI Tweaks are Done.

--- FoodCollector/FCPublishRootVC.swift
+++ FoodCollector/FCPublishRootVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Foundation
+import QuartzCore
+
 
 ///
 /// show all user created publication: live and expired.
@@ -51,19 +53,26 @@ class FCPublishRootVC : UIViewController, UICollectionViewDelegate, UICollection
         let pubTitle = publication.title
         let locDateString = FCDateFunctions.localizedDateStringShortStyle(publication.endingDate)
         
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(reusableId, forIndexPath: indexPath) as FCPublishRootVCCustomCollectionViewCell
+        
         if FCDateFunctions.PublicationDidExpired(publication.endingDate){
             status = "Not Active" // Localizable string
             statusImg = UIImage(named: "Red-dot")!
+            //cell.backgroundColor = UIColor(red: 1.00, green: 0.81, blue: 0.82, alpha: 1.00)
         }
         else {
             status = "Active (until: \(locDateString))" // Localizable string
             statusImg = UIImage(named: "Green-dot")!
+            //cell.backgroundColor = UIColor(red: 0.86, green: 1.00, blue: 0.80, alpha: 1.00)
+
         }
-        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(reusableId, forIndexPath: indexPath) as FCPublishRootVCCustomCollectionViewCell
+        
         
         cell.FCPublisherEventTitle.text = pubTitle
         cell.FCPublisherEventStatus.text = status
         cell.FCPublisherEventStatusIcon.image = statusImg
+        //cell.layer.borderWidth = 1.0
+        //cell.layer.borderColor = UIColor.grayColor().CGColor
         return cell
         
     }
