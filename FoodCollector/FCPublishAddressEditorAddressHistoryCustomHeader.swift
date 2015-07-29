f84f58d034b358c2aad25969216f7bc42de052c5
tsboris <boris@tsbmob.com>
2015-07-30 01:03:41 +0300

    Finished the redesigned address screen. Waiting for Irina's Approval.

---
+++ FoodCollector/FCPublishAddressEditorAddressHistoryCustomHeader.swift
@@ -0,0 +1,24 @@
+//
+//  FCPublishAddressEditorAddressHistoryCustomHeader.swift
+//  FoodCollector
+//
+//  Created by Boris Tsigelman on 30/07/15.
+//  Copyright (c) 2015 Boris Tsigelman. All rights reserved.
+//
+
+import UIKit
+
+class FCPublishAddressEditorAddressHistoryCustomHeader: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
