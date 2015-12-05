a04676bb35c3bf420767329bd750c32c65b727d0
Guy Freedman <gogalefree@gmail.com>
2015-12-05 15:45:42 +0200

    v1.0.8b17 added tapGestureRecognizer to date picker cell changed minimum date to now

--- FoodCollector/PublicationEditorTVCDatePickerCustomCell.swift
+++ FoodCollector/PublicationEditorTVCDatePickerCustomCell.swift
@@ -31,7 +31,7 @@ class PublicationEditorTVCDatePickerCustomCell: UITableViewCell {
                 print("DatePicker Date: \(cellData?.userData as! NSDate)")
                 print("DatePicker Minimum Date: \(minimumDate?.userData as? NSDate)")
                 self.cellDatePicker.setDate(cellData?.userData as! NSDate, animated: false)
-                self.cellDatePicker.minimumDate = minimumDate?.userData as? NSDate
+                self.cellDatePicker.minimumDate = NSDate() //minimumDate?.userData as? NSDate
             }
         }
     }
@@ -43,7 +43,25 @@ class PublicationEditorTVCDatePickerCustomCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        addTapRecognizer()
         // Initialization code
+    }
+    
+    func addTapRecognizer() {
+
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: "datePickerTapped")
+        tapRecognizer.delegate = self
+        self.cellDatePicker.addGestureRecognizer(tapRecognizer)
+    }
+    
+    func datePickerTapped() {
+        if let delegate = self.delegate {
+            delegate.closeDatePicker()
+        }
+    }
+
+    override func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
