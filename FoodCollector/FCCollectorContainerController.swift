4b97e47e4acde1966e68993367955f611b8bafb6
Guy Freedman <gogalefree@gmail.com>
2015-12-22 23:37:34 +0200

    v 1.0.9b1 implement activity center animation for LTR & RTL

--- FoodCollector/FCCollectorContainerController.swift
+++ FoodCollector/FCCollectorContainerController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class FCCollectorContainerController: UIViewController, CollectorVCSlideDelegate {
     
+    @IBOutlet weak var mapNavigationControllerLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mapNavigationControllerTrailingConstraint: NSLayoutConstraint!
+
+    let kCollectorMapVCFraction : CGFloat = 0.85
+    let kConstraintsTotalPadding: CGFloat = 20
+    let kViewsTotalPaddind      : CGFloat = 16
+    
     var activityCenterNavigationController: UINavigationController!
     var collectorRootNavigationController: UINavigationController!
     var activityCenterPresented = false
-    var kCollectorMapVCFraction: CGFloat = 0.85
     
     var tabBarVisibleOrigin: CGPoint!
     var tabBarHiddenOrigin: CGPoint!
@@ -22,24 +28,28 @@ class FCCollectorContainerController: UIViewController, CollectorVCSlideDelegate
     
     var statusBarHidden: Bool = false
     
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         activityCenterNavigationController = self.storyboard?.instantiateViewControllerWithIdentifier("activityCenterNavController") as! UINavigationController
         
-        collectorRootNavigationController = self.storyboard?.instantiateViewControllerWithIdentifier("collectorRootNavController") as! UINavigationController
-        
         self.addChildViewController(activityCenterNavigationController)
         activityCenterNavigationController.view.frame = self.view.bounds
-        activityCenterNavigationController.view.frame.size.width = kCollectorMapVCFraction * CGRectGetWidth(self.view.bounds)
+        
         self.view.addSubview(self.activityCenterNavigationController.view)
         self.activityCenterNavigationController.didMoveToParentViewController(self)
+        self.view.sendSubviewToBack(self.activityCenterNavigationController.view)
         
-        self.addChildViewController(self.collectorRootNavigationController)
-        collectorRootNavigationController.view.frame = self.view.bounds
-        self.view.addSubview(collectorRootNavigationController.view)
-        collectorRootNavigationController.didMoveToParentViewController(self)
+      
+        for vc in self.childViewControllers {
+            if vc is CollectorMapVCNavController {
+                collectorRootNavigationController = vc as? UINavigationController
+                break
+            }
+        }
         
         let collectorRootVC = collectorRootNavigationController.viewControllers[0] as! FCCollectorRootVC
         collectorRootVC.delegate = self
@@ -63,15 +73,29 @@ class FCCollectorContainerController: UIViewController, CollectorVCSlideDelegate
         //inform collector root vc that activity center is presented
         let collectorRootVC = collectorRootNavigationController.viewControllers[0] as! FCCollectorRootVC
         collectorRootVC.isPresentingActivityCenter = true
+        self.mapNavigationControllerLeadingConstraint.constant = self.collectorMapHiddenOrigin.x - kConstraintsTotalPadding
+        self.mapNavigationControllerTrailingConstraint.constant = -(self.collectorMapHiddenOrigin.x - kConstraintsTotalPadding)
+        let leading = self.mapNavigationControllerLeadingConstraint.constant
         
         UIView.animateWithDuration(0.2, animations: { () -> Void in
             
-            self.collectorRootNavigationController.view.frame.origin = self.collectorMapHiddenOrigin
-            self.tabBarController?.tabBar.frame.origin = self.tabBarHiddenOrigin
+            
+            self.view.layoutIfNeeded()
+            
+            if #available(iOS 9.0, *) {
+                if UIView.userInterfaceLayoutDirectionForSemanticContentAttribute(self.view.semanticContentAttribute) == .LeftToRight {
+                    self.tabBarController?.tabBar.frame.origin.x = leading + self.kViewsTotalPaddind
+                    
+                } else {
+                    self.tabBarController?.tabBar.frame.origin.x = -leading - self.kViewsTotalPaddind
+
+                }
+            } else {
+                // Fallback on earlier versions
+            }
             
             }) { (completion) -> Void in
                 activityCenterVC.displaySections()
-        //        self.showStatusBar(false)
         }
     }
     
@@ -85,13 +109,16 @@ class FCCollectorContainerController: UIViewController, CollectorVCSlideDelegate
         
         UIView.animateWithDuration(0.2, animations: { () -> Void in
             
-            self.collectorRootNavigationController.view.center = self.view.center
-            self.tabBarController?.tabBar.frame.origin = self.tabBarVisibleOrigin
+            self.mapNavigationControllerLeadingConstraint.constant = -self.kConstraintsTotalPadding
+            self.mapNavigationControllerTrailingConstraint.constant = -self.kConstraintsTotalPadding
+            self.view.layoutIfNeeded()
+            self.tabBarController?.tabBar.frame.origin.x = 0
+            
             }){ (completion) -> Void in
-      //          self.showStatusBar(true)
         }
     }
     
+   
     func definePointsWithRect(containerBounds: CGRect) {
         
         let collectorMapHiddenOriginX = ceil(containerBounds.width * kCollectorMapVCFraction)
@@ -106,38 +133,7 @@ class FCCollectorContainerController: UIViewController, CollectorVCSlideDelegate
         tabBarHiddenOrigin = CGPointMake(tabBarHiddenOriginX, tabBarHiddenOriginY)
     }
     
-    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
-        
-        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
-        let boundsRect = CGRectMake(0, 0, size.width, size.height)
-        self.definePointsWithRect(boundsRect)
-        
-        coordinator.animateAlongsideTransition({ (context) -> Void in
-            
-            self.activityCenterNavigationController.view.frame = CGRectMake(0, 0, size.width, size.height)
-            self.activityCenterNavigationController.view.frame.size.width = self.kCollectorMapVCFraction * size.width
-            
-            if self.activityCenterPresented {
-                self.showActivityCenter()
-            }
-            
-            }, completion: { (context) -> Void in})
-    }
-    
-//    override func prefersStatusBarHidden() -> Bool {
-//        return statusBarHidden
-//    }
-    
-//    func showStatusBar(show: Bool) {
-//        
-//        UIView.animateWithDuration(0.3, animations: { () -> Void in
-//            self.statusBarHidden = !show
-//            self.setNeedsStatusBarAppearanceUpdate()
-//        })
-//    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
