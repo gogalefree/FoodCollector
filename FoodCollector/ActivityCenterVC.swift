409a4e5432fc987628bb7408a4a69d8ca2573594
tsboris <boris@tsbmob.com>
2016-03-05 13:44:43 +0200

    Initial commit of graphics and basic classes

--- FoodCollector/ActivityCenterVC.swift
+++ FoodCollector/ActivityCenterVC.swift
@@ -8,22 +8,42 @@
 
 import UIKit
 
-class ActivityCenterVC: UIViewController, UIGestureRecognizerDelegate {
+let sideMenuBtnTitleMyShares  = NSLocalizedString("My Shares", comment:"Title for a side menu button")
+let sideMenuBtnTitleAllEvents = NSLocalizedString("All Events", comment:"Title for a side menu button")
+let sideMenuBtnTitleGroups    = NSLocalizedString("Groups", comment:"Title for a side menu button")
+let sideMenuBtnTitleSettings  = NSLocalizedString("Settings", comment:"Title for a side menu button")
+let sideMenuBtnTitleContactUs = NSLocalizedString("Contact Us", comment:"Title for a side menu button")
+let sideMenuBtnTitleAbout     = NSLocalizedString("About", comment:"Title for a side menu button")
+
+class ActivityCenterVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var leftSwipeGesture: UISwipeGestureRecognizer!
     
     @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var userIdentityProviderName: UILabel!
     
-
+    
+    @IBOutlet weak var sideMenuTable: UITableView!
+    
+    var buttunsTitleArray: [String] = [];
+    var buttunsImageArray: [UIImage] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("ActivityCenterVC viewDidLoad()")
         leftSwipeGesture.addTarget(self, action: "leftSwipeAction:")
         
         userIdentityProviderName.text = User.sharedInstance.loginData?.identityProviderUserName
         displayUserProfileImage()
+        
+        sideMenuTable.delegate = self
+        sideMenuTable.dataSource = self
+        
+        createButtunsTitleArray()
+        createButtunsImageArray()
+        
         
     }
 
@@ -37,6 +57,139 @@ class ActivityCenterVC: UIViewController, UIGestureRecognizerDelegate {
         let container = self.navigationController?.parentViewController as! FCCollectorContainerController
         container.collectorVCWillSlide()
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("buttunsTitleArray.count: \(buttunsTitleArray.count)")
+        return buttunsTitleArray.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.row == 3 {
+            return 3
+        }
+        
+        return 27
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        /// Cells Structure
+        // 0 - My Shares
+        // 1 - All Events
+        // 2 - Groups
+        // 3 - -- Seperator --
+        // 4 - Settings
+        // 5 - Contact Us
+        // 6 - About
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("sideMenuItemCell") as! sideMenuItemTVCell
+        //cell.articles = self.articles?[indexPath.row]
+        cell.sideMenuIcon.image = buttunsImageArray[indexPath.row]
+        cell.sideMenuTitle.text = buttunsTitleArray[indexPath.row]
+        if indexPath.row == 3 {
+            cell.backgroundColor = UIColor.grayColor()
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        // do something
+    }
+    
+    
+    
+    
+    func createButtunsTitleArray() {
+        // The empty string in the array represenet the seperator.
+        // Without it, the index of the titles in the array will be off sync
+        // with the index of the cells in the table.
+        
+        buttunsTitleArray = [sideMenuBtnTitleMyShares, sideMenuBtnTitleAllEvents, sideMenuBtnTitleGroups, "", sideMenuBtnTitleSettings, sideMenuBtnTitleContactUs, sideMenuBtnTitleAbout]
+    }
+    
+    func createButtunsImageArray() {
+        // The empty image in the array represenet the seperator.
+        // Without it, the index of the icons in the array will be off sync
+        // with the index of the cells in the table.
+        
+        let sideMenuIconMyShares     = UIImage(named: "MyShares")
+        let sideMenuIconAllEvents    = UIImage(named: "AllEvents")
+        let sideMenuIconGroups       = UIImage(named: "Groups")
+        let sideMenuIconEmptyImage   = UIImage()
+        let sideMenuIconSettings     = UIImage(named: "Settings")
+        let sideMenuIconContactUs    = UIImage(named: "ContactUs")
+        let sideMenuIconAbout        = UIImage(named: "About")
+        
+        if let image = sideMenuIconMyShares {
+            buttunsImageArray.append(image)
+        }
+        if let image = sideMenuIconAllEvents {
+            buttunsImageArray.append(image)
+        }
+        if let image = sideMenuIconGroups {
+            buttunsImageArray.append(image)
+        }
+
+        buttunsImageArray.append(sideMenuIconEmptyImage)
+        
+        if let image = sideMenuIconSettings {
+            buttunsImageArray.append(image)
+        }
+        if let image = sideMenuIconContactUs {
+            buttunsImageArray.append(image)
+        }
+        if let image = sideMenuIconAbout {
+            buttunsImageArray.append(image)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func menuViewTapped(sender: UITapGestureRecognizer) {
         
